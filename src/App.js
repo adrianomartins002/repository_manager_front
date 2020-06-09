5bced0594e3041f6ed406a9141744fc177a1f156
Adriano Martins <adrianomartins002@gmail.com>
2020-06-09 15:08:23 -0300

    finish challenge

--- src/App.js
+++ src/App.js
@@ -1,29 +1,71 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import "./styles.css";
+import api from "./services/api";
 
 function App() {
+
+  const [repositories, setRepositories] = useState([]);
+
+
   async function handleAddRepository() {
-    // TODO
+    const title = document.getElementById("title").value;
+    const url = document.getElementById("url").value;
+    const techs = document.getElementById("techs").value;
+
+    const result = await api.post('/repositories', {
+      title,
+      url,
+      techs,
+    });
+
+    setRepositories([...repositories, result.data]);
+
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    const result = await api.delete(`/repositories/${id}`);
+    if (result.status === 204)
+      setRepositories([...repositories.filter(repo => repo.id !== id)]);
   }
+
+  async function searchRepos() {
+    const data = await (await api.get('/repositories')).data;
+    setRepositories(data);
+  }
+
+  useEffect(() => {
+    searchRepos();
+  }, [])
+
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+        {repositories.length > 0 ?
+          repositories.map(repo =>
+            <li key={repo.title}>
+              {repo.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
+              <button onClick={() => handleRemoveRepository(repo.id)}>
+                Remover
           </button>
-        </li>
+            </li>
+          )
+          :
+          null
+        }
       </ul>
-
-      <button onClick={handleAddRepository}>Adicionar</button>
+      <div style={{
+        marginTop: "20px"
+      }}>
+        <form className="formulario" action="" >
+          <input id="title" type="text" placeholder="title" />
+          <input id="url" type="text" placeholder="url" />
+          <input id="techs" type="text" placeholder="techs" />
+        </form>
+        <button onClick={handleAddRepository}>Adicionar</button>
+      </div>
     </div>
   );
 }
